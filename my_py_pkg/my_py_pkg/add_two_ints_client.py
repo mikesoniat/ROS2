4f36314824e1ee29e3cbf52eec68c4e51568dda5
mikesoniat <mike.soniat@mjobtime.com>
2023-06-12 16:45:40 -0500

    add server and client for add two ints service

--- my_py_pkg/my_py_pkg/add_two_ints_client.py
+++ my_py_pkg/my_py_pkg/add_two_ints_client.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node 
+from functools import partial 
+from example_interfaces.srv import AddTwoInts
 
-class MyCustomNode(Node): # CLASS NAME
+class AddTwoIntsClientNode(Node): # CLASS NAME
 
     def __init__(self):
-        super().__init__("py_test") # NODE NAME
+        super().__init__("add_two_inst_client")
+        self.call_add_two_ints_server(6, 7)
+        self.call_add_two_ints_server(5, 2)
+        self.call_add_two_ints_server(3, 1)
+
+    def call_add_two_ints_server(self, a, b):
+        client = self.create_client(AddTwoInts, "add_two_ints")
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for Server Add Two Ints") 
+
+        request = AddTwoInts.Request()
+        request.a = a 
+        request.b = b 
+
+        future = client.call_async(request) 
+        future.add_done_callback(partial(self.callback_call_add_two_ints, a=a, b=b))
+
+    def callback_call_add_two_ints(self, future, a, b):
+        try:
+            response = future.result()
+            self.get_logger().info(str(a) + " + " + str(b) + " = " + str(response.sum))
+        except Exception as e:
+            self.get_logger().error("Service call failed %r" % (e,))
 
 
 def main(args=None):
     rclpy.init(args=args) # initialize ROS communication
-    node = MyCustomNode() # CLASS NAME
+    node = AddTwoIntsClientNode() 
     rclpy.spin(node)
     rclpy.shutdown() 
 
