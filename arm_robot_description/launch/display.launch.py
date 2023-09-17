632c36c6d9b15e9b1e9ba9d3061a2faf325d29de
mikesoniat <mike.soniat@mjobtime.com>
2023-09-17 12:27:33 -0500

    added xacro

--- arm_robot_description/launch/display.launch.py
+++ arm_robot_description/launch/display.launch.py
@@ -8,7 +8,7 @@ from ament_index_python.packages import get_package_share_path
 def generate_launch_description():
     
     urdf_path = os.path.join(get_package_share_path('arm_robot_description'),
-                             'urdf', 'arm_robot.urdf')
+                             'urdf', 'arm_robot.urdf.xacro')
     
     rviz_config_path = os.path.join(get_package_share_path('arm_robot_description'),
                              'rviz', 'urdf_config.rviz')    
